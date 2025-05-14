90a6915db7a8b26336b5b984d20d003c38a91b98
Etex99 <emma99.kem@gmail.com>
2025-05-14 18:36:24 +0300

    exercise 5.7

--- src/components/Blog.jsx
+++ src/components/Blog.jsx
@@ -1,7 +1,26 @@
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>  
-)
+import { useState } from 'react'
+
+const Blog = ({ blog }) => {
+  const [showDetails, setShowDetails] = useState(false)
+  
+  const toggle = () => (
+    <button onClick={() => {setShowDetails(!showDetails)}}>{ showDetails ? 'hide' : 'view' }</button>
+  )
+
+  return (
+  <div className='blog'>
+    {blog.title} {blog.author} {toggle()}
+    {showDetails ?
+    (
+      <>
+        <p className='blog-detail'>{blog.url}</p>
+        <p className='blog-detail'>likes: {blog.likes} <button>like</button></p>
+        <p className='blog-detail'>{blog.user.username}</p>
+      </>
+    ) : <></>
+    }
+  </div>
+  )  
+}
 
 export default Blog
